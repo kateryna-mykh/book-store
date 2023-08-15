94d050c08d5d1c6f7abaacaba4cc258855bc10b1
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2023-08-15 20:56:26 +0300

    fixed constraint for price field

--- src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -2,6 +2,7 @@ package bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Getter;
 
@@ -14,7 +15,8 @@ public class CreateBookRequestDto {
     @NotNull
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
+    @Min(1)
     private BigDecimal price;
     private String description;
     private String coverImage;
