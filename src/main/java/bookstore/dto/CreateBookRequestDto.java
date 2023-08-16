0df15e1981838aeba21fa0f2848f93519ba0a080
Kateryna Mykhailovska <112807544+kateryna-mykh@users.noreply.github.com>
2023-08-16 14:33:11 +0300

    hw-validation (#4)
    * fixed constraint for price field
    * added annotation: URL, notNull->notBlank for required string fields

--- src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,27 @@
 package bookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Getter;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
     private String isbn;
+    @NotNull
+    @Positive
+    @Min(1)
     private BigDecimal price;
     private String description;
+    @NotBlank
+    @URL
     private String coverImage;
 }
